494e1cfbc805c69af88063df22c69b31aa171012
gaurav <gaurav.suryawanshi@neosofttech.com>
2019-11-01 12:43:33 +0530

    active link on admin panel

--- resources/views/layouts/script.blade.php
+++ resources/views/layouts/script.blade.php
@@ -74,6 +74,15 @@
          $(this).parents(".control-group").remove();
      });
 
+    var url = window.location;
+
+    $('ul.sidebar-menu a').filter(function() {
+  	   return this.href == url;
+     }).parent().addClass('active');
+
+     $('ul.treeview-menu a').filter(function() {
+	 return this.href == url;
+}).parentsUntil(".sidebar-menu > .treeview-menu").addClass('active');
 
     });
 
